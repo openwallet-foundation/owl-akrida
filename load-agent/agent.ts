a09a5beb671ec86e192eeea72ab8127f69e34c7e
AmarjeetAkv <vermaamarjeetkumar0@gmail.com>
2024-10-24 22:31:11 +0530

    changed Network config of indyVdr
    Signed-off-by: AmarjeetAkv <vermaamarjeetkumar0@gmail.com>

--- load-agent/agent.ts
+++ load-agent/agent.ts
@@ -91,7 +91,6 @@ const initializeAgent = async (withMediation, port, agentConfig = null) => {
   // configuration and the label. It also sets the mediator invitation url,
   // because this is most likely required in a mobile environment.
   try {
-    // console.log('initializeAgent start');
   let mediation_url = config.mediation_url
   let endpoints = ['http://' + config.agent_ip + ':' + port]
 
@@ -118,26 +117,18 @@ const initializeAgent = async (withMediation, port, agentConfig = null) => {
   const anonCredsProofFormatService = new AnonCredsProofFormatService()
 
 
-  // console.log('ledger: ',config.ledger);
   
   let modules = {
     indyVdr: new IndyVdrModule({
       indyVdr,
-      networks: [
-        {
-          isProduction: false,
-          indyNamespace: 'bcovrin:test',
-          genesisTransactions: `{"reqSignature":{},"txn":{"data":{"data":{"alias":"Node1","blskey":"4N8aUNHSgjQVgkpm8nhNEfDf6txHznoYREg9kirmJrkivgL4oSEimFF6nsQ6M41QvhM2Z33nves5vfSn9n1UwNFJBYtWVnHYMATn76vLuL3zU88KyeAYcHfsih3He6UHcXDxcaecHVz6jhCYz1P2UZn2bDVruL5wXpehgBfBaLKm3Ba","blskey_pop":"RahHYiCvoNCtPTrVtP7nMC5eTYrsUA8WjXbdhNc8debh1agE9bGiJxWBXYNFbnJXoXhWFMvyqhqhRoq737YQemH5ik9oL7R4NTTCz2LEZhkgLJzB3QRQqJyBNyv7acbdHrAT8nQ9UkLbaVL9NBpnWXBTw4LEMePaSHEw66RzPNdAX1","client_ip":"138.197.138.255","client_port":9702,"node_ip":"138.197.138.255","node_port":9701,"services":["VALIDATOR"]},"dest":"Gw6pDLhcBcoQesN72qfotTgFa7cbuqZpkX3Xo6pLhPhv"},"metadata":{"from":"Th7MpTaRZVRYnPiabds81Y"},"type":"0"},"txnMetadata":{"seqNo":1,"txnId":"fea82e10e894419fe2bea7d96296a6d46f50f93f9eeda954ec461b2ed2950b62"},"ver":"1"}{"reqSignature":{},"txn":{"data":{"data":{"alias":"Node2","blskey":"37rAPpXVoxzKhz7d9gkUe52XuXryuLXoM6P6LbWDB7LSbG62Lsb33sfG7zqS8TK1MXwuCHj1FKNzVpsnafmqLG1vXN88rt38mNFs9TENzm4QHdBzsvCuoBnPH7rpYYDo9DZNJePaDvRvqJKByCabubJz3XXKbEeshzpz4Ma5QYpJqjk","blskey_pop":"Qr658mWZ2YC8JXGXwMDQTzuZCWF7NK9EwxphGmcBvCh6ybUuLxbG65nsX4JvD4SPNtkJ2w9ug1yLTj6fgmuDg41TgECXjLCij3RMsV8CwewBVgVN67wsA45DFWvqvLtu4rjNnE9JbdFTc1Z4WCPA3Xan44K1HoHAq9EVeaRYs8zoF5","client_ip":"138.197.138.255","client_port":9704,"node_ip":"138.197.138.255","node_port":9703,"services":["VALIDATOR"]},"dest":"8ECVSk179mjsjKRLWiQtssMLgp6EPhWXtaYyStWPSGAb"},"metadata":{"from":"EbP4aYNeTHL6q385GuVpRV"},"type":"0"},"txnMetadata":{"seqNo":2,"txnId":"1ac8aece2a18ced660fef8694b61aac3af08ba875ce3026a160acbc3a3af35fc"},"ver":"1"}{"reqSignature":{},"txn":{"data":{"data":{"alias":"Node3","blskey":"3WFpdbg7C5cnLYZwFZevJqhubkFALBfCBBok15GdrKMUhUjGsk3jV6QKj6MZgEubF7oqCafxNdkm7eswgA4sdKTRc82tLGzZBd6vNqU8dupzup6uYUf32KTHTPQbuUM8Yk4QFXjEf2Usu2TJcNkdgpyeUSX42u5LqdDDpNSWUK5deC5","blskey_pop":"QwDeb2CkNSx6r8QC8vGQK3GRv7Yndn84TGNijX8YXHPiagXajyfTjoR87rXUu4G4QLk2cF8NNyqWiYMus1623dELWwx57rLCFqGh7N4ZRbGDRP4fnVcaKg1BcUxQ866Ven4gw8y4N56S5HzxXNBZtLYmhGHvDtk6PFkFwCvxYrNYjh","client_ip":"138.197.138.255","client_port":9706,"node_ip":"138.197.138.255","node_port":9705,"services":["VALIDATOR"]},"dest":"DKVxG2fXXTU8yT5N7hGEbXB3dfdAnYv1JczDUHpmDxya"},"metadata":{"from":"4cU41vWW82ArfxJxHkzXPG"},"type":"0"},"txnMetadata":{"seqNo":3,"txnId":"7e9f355dffa78ed24668f0e0e369fd8c224076571c51e2ea8be5f26479edebe4"},"ver":"1"}{"reqSignature":{},"txn":{"data":{"data":{"alias":"Node4","blskey":"2zN3bHM1m4rLz54MJHYSwvqzPchYp8jkHswveCLAEJVcX6Mm1wHQD1SkPYMzUDTZvWvhuE6VNAkK3KxVeEmsanSmvjVkReDeBEMxeDaayjcZjFGPydyey1qxBHmTvAnBKoPydvuTAqx5f7YNNRAdeLmUi99gERUU7TD8KfAa6MpQ9bw","blskey_pop":"RPLagxaR5xdimFzwmzYnz4ZhWtYQEj8iR5ZU53T2gitPCyCHQneUn2Huc4oeLd2B2HzkGnjAff4hWTJT6C7qHYB1Mv2wU5iHHGFWkhnTX9WsEAbunJCV2qcaXScKj4tTfvdDKfLiVuU2av6hbsMztirRze7LvYBkRHV3tGwyCptsrP","client_ip":"138.197.138.255","client_port":9708,"node_ip":"138.197.138.255","node_port":9707,"services":["VALIDATOR"]},"dest":"4PS3EDQ3dW1tci1Bp6543CfuuebjFrg36kLAUcskGfaA"},"metadata":{"from":"TWwCRQRZ2ZHMJFn9TzLp7W"},"type":"0"},"txnMetadata":{"seqNo":4,"txnId":"aa5e817d7cc626170eca175822029339a444eb0ee8f0bd20d3b0b76e566fb008"},"ver":"1"}`,
-          connectOnStartup: true,
-        }
-      ]
+      networks: [config.ledger]
     }),
     askar: new AskarModule({
       ariesAskar,
     }),
-    mediator: new MediatorModule({
-      autoAcceptMediationRequests: true,
-    }),
+    // mediator: new MediatorModule({
+    //   autoAcceptMediationRequests: true,
+    // }),
     mediationRecipient: new MediationRecipientModule({
       mediatorInvitationUrl: mediation_url,
       mediatorPickupStrategy: MediatorPickupStrategy.Implicit,
@@ -190,30 +181,20 @@ const initializeAgent = async (withMediation, port, agentConfig = null) => {
   })
   
     const apps = express()
-    // console.log('Apps:',apps);
     const wsTransport = new WsOutboundTransport()
     const httpTransport = new HttpOutboundTransport()
-    // const socketServer = new WebSocketServer({ port:4003,host:'127.0.0.1' })
-    // console.log('socketServer:',socketServer)
-    // const wsInboundTransport = new WsInboundTransport({server:socketServer})
     const httpInbound = new HttpInboundTransport({
       port:4002,
       app:apps,
       path:'/'
     })
 
-    // console.log('wsTransport: ',wsTransport)
-    // console.log('httpTransport: ',httpTransport)
-    // console.log('httpInbound: ',httpInbound)
-
-    agent.registerOutboundTransport(wsTransport)
-    agent.registerOutboundTransport(httpTransport)
-  // // console.log('Agent at line 202',agent);
+  
   // Register a simple `WebSocket` outbound transport
-  // agent.registerOutboundTransport(new WsOutboundTransport())
+    agent.registerOutboundTransport(wsTransport)
 
-  // // Register a simple `Http` outbound transport
-  // agent.registerOutboundTransport(new HttpOutboundTransport())
+  // Register a simple `Http` outbound transport
+    agent.registerOutboundTransport(httpTransport)
 
   if (withMediation) {
     // wait for medation to be configured
@@ -269,10 +250,7 @@ const initializeAgent = async (withMediation, port, agentConfig = null) => {
       throw 'Mediator timeout!'
     }
   } else {
-    // console.log('withMediation elseee',port)
-    // agent.registerInboundTransport(
-    //   new HttpInboundTransport({ port: port })
-    // )
+
     agent.registerInboundTransport(httpInbound);
     await agent.initialize()
   }
@@ -280,12 +258,12 @@ const initializeAgent = async (withMediation, port, agentConfig = null) => {
   return [agent, agentConfig]
 
   } catch (error) {
-    // console.log('Error at intialize agent',error);
+
     process.stderr.write('******** ERROR Error at intialize agent'+ '\n' + error + '\n')
   }
   
 }
-// console.log('below initializeAgent')
+
 const pingMediator = async (agent) => {
   // Find mediator
 
@@ -479,14 +457,12 @@ let receiveCredential = async (agent) => {
 
     switch (payload.credentialRecord.state) {
       case CredentialState.OfferReceived:
-        //console.log('received a credential')
         // custom logic here
         await agent.credentials.acceptOffer({
           credentialRecordId: payload.credentialRecord.id,
         })
         break
       case CredentialState.CredentialReceived:
-        //console.log(`Credential for credential id ${payload.credentialRecord.id} is accepted`)
         // For demo purposes we exit the program here.
 
         agent.events.off(
--- load-agent/config.js
+++ load-agent/config.js
@@ -1,12 +1,19 @@
 require("dotenv").config();
-
+const fs = require('fs');
 const CANdy = ``;
-
 let ledger = {};
-
+const ReadGenesisTransactions = (networks) => {
+  try {
+    const fileContents = fs.readFileSync(networks, 'utf-8');
+    const genesisTransactions = fileContents;
+    return genesisTransactions
+  } catch (error) {
+    process.stderr.write('******** Error at ReadingGenesisTransactions'+ '\n' +error+ '\n')
+  }
+}
 if (process.env.LEDGER == "bcovrin") {
   ledger = {
-    genesisPath: "./networks/BCovrintest.txn",
+    genesisTransactions: ReadGenesisTransactions("./networks/BCovrintest.txn"),
     id: "BCovrinTest",
     indyNamespace: 'bcovrin:test',
     isProduction: false,
@@ -14,7 +21,7 @@ if (process.env.LEDGER == "bcovrin") {
   };
 } else if (process.env.LEDGER == "candy") {
   ledger = {
-    genesisPath: "./networks/CANdy.txn",
+    genesisTransactions: ReadGenesisTransactions("./networks/CANdy.txn"),
     id: "CANdyDev",
     indyNamespace: 'did:indy:candy:dev',
     isProduction: false,
@@ -22,14 +29,13 @@ if (process.env.LEDGER == "bcovrin") {
   };
 } else if (process.env.LEDGER == "indicio") {
   ledger = {
-    genesisPath: "./networks/indicio-test.txn",
+    genesisTransactions: ReadGenesisTransactions("./networks/indicio-test.txn"),
     id: "IndicioTest",
     indyNamespace: 'did:indy:indicio:test',
     isProduction: false,
     connectOnStartup: true,
   };
 }
-
 exports.mediation_url = process.env.MEDIATION_URL;
 exports.agent_ip = process.env.AGENT_IP;
 exports.verified_timeout_seconds = (process.env.VERIFIED_TIMEOUT_SECONDS || 120)
